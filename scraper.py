56d9e6053a0f3e092222c4d237fd3e94c4b7c121
thucchi-cs <thucchi.code@gmail.com>
2025-06-09 23:40:19 -0500

    linkedin scrape with selenium

--- scraper.py
+++ scraper.py
@@ -1,26 +1,13 @@
-from bs4 import BeautifulSoup
-import requests
-import csv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-file = open("scraped.csv", "w")
-writer = csv.writer(file)
-writer.writerow(["QUOTES", "AUTHORS"])
+driver = webdriver.Chrome()
+driver.get("https://www.linkedin.com/company/comvest-partners/about/")
+print(driver.current_url)
 
-page = requests.get("https://quotes.toscrape.com/")
-soup = BeautifulSoup(page.text, "html.parser")
-
-quotes = soup.findAll("span", attrs={"class":"text"})
-authors = soup.findAll("small", attrs={"class":"author"})
-
-for q,a in zip(quotes, authors):
-    print(q.text, a.text)
-    writer.writerow([q.text, a.text])
-file.close()
+name = driver.find_element(By.TAG_NAME, "h1")
+print(name.text)
 
 
-# Same thing - different method
-# quotes = soup.findAll("div", attrs={"class":"quote"})
-# for q in quotes:
-#     t = q.findAll("span", attrs={"class":"text"})
-#     a = q.findAll("small", attrs={"class":"author"})
-#     print(t[0].text, a[0].text)
+# break-words white-space-pre-wrap t-black--light text-body-medium
