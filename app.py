3eba8081fe0b54bebb9ac5b683f535fef40d84e6
thucchi-cs <thucchi.code@gmail.com>
2025-06-26 01:20:10 -0500

    error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import scraper
 
 app = Flask(__name__)
@@ -11,26 +11,29 @@ def index():
 
 @app.route("/load", methods=["POST"])
 def load():
-    data = request.get_json()
-    data["min_followers"] = 0 if data["min_followers"] == "" else int(data["min_followers"])
-    data["min_employees"] = 0 if data["min_employees"] == "" else int(data["min_employees"])
-    data["max_followers"] = 0 if data["max_followers"] == "" else int(data["max_followers"])
-    data["max_employees"] = 0 if data["max_employees"] == "" else int(data["max_employees"])
-    
-    for i in range(len(data["keywords"])):
-        data["keywords"][i] = data['keywords'][i].lower()
-    
-    print(data)
-    driver = scraper.open_driver()
-    companies = data["companies"]
-    results = {}
-    for c in companies:
-        url = c.lower()
-        url = url.replace(" ", "-")
-        url = f"https://www.linkedin.com/company/{url}/"
-        fit = scraper.check_fit(url, driver, data)
-        results[c] = fit
+    try:
+        data = request.get_json()
+        data["min_followers"] = 0 if data["min_followers"] == "" else int(data["min_followers"])
+        data["min_employees"] = 0 if data["min_employees"] == "" else int(data["min_employees"])
+        data["max_followers"] = 0 if data["max_followers"] == "" else int(data["max_followers"])
+        data["max_employees"] = 0 if data["max_employees"] == "" else int(data["max_employees"])
+        
+        for i in range(len(data["keywords"])):
+            data["keywords"][i] = data['keywords'][i].lower()
+        
+        print(data)
+        driver = scraper.open_driver()
+        companies = data["companies"]
+        results = {}
+        for c in companies:
+            url = c.lower()
+            url = url.replace(" ", "-")
+            url = f"https://www.linkedin.com/company/{url}/"
+            fit = scraper.check_fit(url, driver, data)
+            results[c] = fit
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
         driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-    driver.close()
-    return results
+        return results
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
